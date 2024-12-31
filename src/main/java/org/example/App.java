29708f60243735deb78939cf67f1928ac664926d
yangwei6 <yangwei6@lixiang.com>
2024-12-31 15:07:08 +0800

    feature(init): 仓库初始化

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        long a = 1L;
+        System.out.println(a);
+    }
+}
